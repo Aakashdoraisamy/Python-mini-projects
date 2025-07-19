a076979f2399abae887478382921a1b216b92405
Aakashdoraisamy <146075602+Aakashdoraisamy@users.noreply.github.com>
2025-07-19 21:51:50 +0530

    Add email sender mini project

---
+++ email_sender/email_sender.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html_template = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['From'] = ''
+email['To'] = 'aakashdorai@gmail.com'
+email['Subject'] = 'Automated Email from Python Script'
+
+email.set_content('This is a fallback message for email clients that do not support HTML.')
+email.add_alternative(html_template.substitute({'name': 'Aakash'}), subtype='html')
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('ad7545448@gmail.com', 'mxzlatatqyenhuem')
+    smtp.send_message(email)
+    print('Email sent successfully!')
+    
